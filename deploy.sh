8a8107306dfb3db829d0f7810e429dd4987a63dd
Jake Overall <joverall22@gmail.com>
2021-08-15 23:08:46 -0600

    prep for bcw tool

--- deploy.sh
+++ deploy.sh
@@ -3,23 +3,20 @@
 # abort on errors
 set -e
 
+# commit current src
+git add .
+git commit -m 'deploy'
+git push
+
 # build
 npm run build
 
 # navigate into the build output directory
-cd dist
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+cd src/.vuepress/dist
 
 git init
-git add -A
+git add .
+git checkout -b gh-pages
+git remote add origin https://github.com/{{ghname}}/fs-journal.git
 git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:{{ghname}}/fs-journal.git main:gh-pages
-
-cd -
+git push --set-upstream -f origin gh-pages
