0f93f34933070f75e23c345e5b8da846693968a6
Jake Overall <joverall22@gmail.com>
2021-09-29 12:26:10 -0600

    testing

--- app/main.js
+++ app/main.js
@@ -8,6 +8,13 @@ function setActiveNavLink() {
 
 function init() {
   setActiveNavLink()
+  // @ts-ignore
+  var sjs = SimpleJekyllSearch({
+    searchInput: document.getElementById('search-input'),
+    resultsContainer: document.getElementById('results-container'),
+    json: '/search.json',
+    searchResultTemplate: '<li><a href="{{ site.url }}{url}">{title}</a></li>'
+  })
 }
 
 
